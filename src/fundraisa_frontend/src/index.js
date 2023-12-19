880263ce58f0c0318b34c2cd7f18b6aff8d3b523
chriss1525 <okothchris15@gmail.com>
2023-12-20 00:11:04 +0300

    try to connect frontend to backend

--- src/fundraisa_frontend/src/index.js
+++ src/fundraisa_frontend/src/index.js
@@ -5,11 +5,14 @@ document.querySelector("form").addEventListener("submit", async (e) => {
   const button = e.target.querySelector("button");
 
   const name = document.getElementById("name").value.toString();
+  const goal = BigInt(document.getElementById("goal").value);
+  const owner = document.getElementById("owner").value.toString();
+  const destination = document.getElementById("destination").value.toString();
 
   button.setAttribute("disabled", true);
 
   // Call the createCampaign function in the backend canister
-  await fundraisa_backend.createCampaign(name, /* add other required parameters */);
+  await fundraisa_backend.createCampaign(name, goal, owner, destination);
 
   // Optionally, you can retrieve campaign details or perform other actions here
 
