777874652e3ff425b752513578f38ec9763ae21c
chriss1525 <okothchris15@gmail.com>
2024-01-26 13:27:27 +0300

    refresh frontend

--- src/fundraisa_frontend/src/index.js
+++ src/fundraisa_frontend/src/index.js
@@ -1,43 +1,19 @@
-// // Import the necessary functions from your declarations
-// import { fundraisa_backend } from "../../declarations/fundraisa_backend";
+import { fundraisa_backend } from "../../declarations/fundraisa_backend";
 
-// // Function to initialize the backend and get the canister ID
-// async function initializeBackend() {
-//   // Call the appropriate function from your declarations to get the canister ID
-//   try {
-//     const canisterId = await fundraisa_backend.getCanisterId();
-//     return canisterId;
-//   } catch (error) {
-//     console.error("Error fetching canister ID:", error);
-//     // Handle the error appropriately
-//     throw error;
-//   }
-// }
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
 
-// // Function to extract query parameters from the URL
-// function getQueryParam(name) {
-//   const urlParams = new URLSearchParams(window.location.search);
-//   return urlParams.get(name);
-// }
+  const name = document.getElementById("name").value.toString();
 
-// // Update the Create Campaign link with the canister ID
-// document.addEventListener('DOMContentLoaded', async function() {
-//   try {
-//     // Initialize the backend and get the canister ID
-//     const canisterId = await initializeBackend();
+  button.setAttribute("disabled", true);
 
-//     // Update the Create Campaign link with the canister ID
-//     const createCampaignLink = document.getElementById('createCampaignLink');
-//     createCampaignLink.href = `create_campaign.html?`;
+  // Interact with foo actor, calling the greet method
+  const greeting = await fundraisa_backend.greet(name);
 
-//     // Get the canister ID from the URL
-//     // const id = getQueryParam('canisterId');
+  button.removeAttribute("disabled");
 
-//     // Open create campaign page
-//     const createCampaignPage = document.getElementById('createCampaignPage');
-//     createCampaignPage.href = `create_campaign.html`;
-//   } catch (error) {
-//     console.error("Initialization error:", error);
-//     // Handle the initialization error appropriately
-//   }
-// });
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
