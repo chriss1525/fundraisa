3cf5943a79cb55871857a7a75a99e7c4be624f24
Achebi <60100857+Achebi@users.noreply.github.com>
2024-02-03 20:32:03 -0500

    Update index.js
    Link front-end and back-end

--- src/fundraisa_frontend/src/index.js
+++ src/fundraisa_frontend/src/index.js
@@ -1,19 +1,54 @@
+// Expose the important parts of main.mo code (fundraisa_backend) to Javascript (index.js)
 import { fundraisa_backend } from "../../declarations/fundraisa_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+// Receive local host's registration objects/values
+const campaignObject = JSON.parse(localStorage.getItem("campaignObject"));
+const personalObject = JSON.parse(localStorage.getItem("personalObject"));
+//console.log(campaignObject); //to be deleted
+//console.log(personalObject); //to be deleted
 
-  const name = document.getElementById("name").value.toString();
+// ADD FUNDRAISING CAMPAIGN DETAILS
+if (campaignObject.length != 0) {
+    // Prepare data (campaignPayload object) to send to backend
+    const campaignGoal = parseFloat(campaignObject.cGoal);
+    let campaignPlayload = {
+        campaignTitle: campaignObject.cTitle,
+        campaignType: campaignObject.cType,
+        campaignDescription: campaignObject.cDescription,
+        campaignGoal: campaignGoal,
+        campaignEndDateTime: campaignObject.cEndDateTime
+    };
+    
+    console.log(campaignPlayload); //to be deleted
+    // Send data to backend
+    await fundraisa_backend.addCampaign(campaignPlayload);
 
-  button.setAttribute("disabled", true);
+    // Delete local host's registration objects/values from user's browser
+    localStorage.removeItem("campaignObject");
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await fundraisa_backend.greet(name);
+    // Get values from backend for display at frontend
+    // # To do
+};
 
-  button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+// ADD USER DETAILS
+if (personalObject.length != 0) {
+    // Prepare data (userPayload object) to send to backend
+    let userPayload = {
+        userName: personalObject.userName,
+        userTelNo: personalObject.userTelNo,
+        yearOfBirth: personalObject.yearOfBirth,
+        contributionDestination: personalObject.userAcNo
+    };
 
-  return false;
-});
+    console.log(userPlayload); //to be deleted
+    // Send data to backend
+    await fundraisa_backend.addUser(userPayload);
+
+    // Delete local host's registration objects/values from user's browser
+    localStorage.removeItem("personalObject");
+
+};
+
+//#To do
+//Retrieve campaing details (campaing ID, Title, Description, goal, time, goal balance etc) from backend for display
