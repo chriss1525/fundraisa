10e477d2f366ab4596c17a1f28eed51a5e1c9f60
chriss1525 <okothchris15@gmail.com>
2024-02-07 08:57:43 +0300

    integrate internet identity signin

--- src/fundraisa_frontend/src/index.jsx
+++ src/fundraisa_frontend/src/index.jsx
@@ -9,7 +9,12 @@ import {
   useNavigate,
 } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Actor, HttpAgent, IDL, Principal } from "@dfinity/agent";
+import { 
+  Actor, 
+  HttpAgent, 
+  IDL, 
+  Principal 
+} from "@dfinity/agent";
 import {
   idlFactory as fundraisa_backend_idl,
   canisterId as fundraisa_backend_canister_id,
@@ -18,12 +23,12 @@ import { fundraisa_backend } from "../../declarations/fundraisa_backend/index";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { Card,
-  CardContent,
-  CardDescription,
+  CardContent,  
   CardFooter,
-  CardHeader,
+  CardDescription,
   CardTitle, } from "./components/ui/card";
 import { Navigate } from "../../../node_modules/react-router-dom/dist/index";
+import { AuthClient } from "@dfinity/auth-client";
 
 
 // Initialize the agent
@@ -36,6 +41,8 @@ const useHistory = () => {
   return navigate;
 };
 
+const authClient = await AuthClient.create();
+
 // Create an actor for the backend
 const fundraisaBackend = Actor.createActor(fundraisa_backend_idl, {
   agent,
@@ -46,13 +53,13 @@ const fundraisaBackend = Actor.createActor(fundraisa_backend_idl, {
 // Landing page make sure listed campaigns are displayed and user can signup/login
 const Landing = () => {
   const [campaigns, setCampaigns] = useState([]);
+  const navigate = useHistory();
 
   useEffect(() => {
     const fetchCampaigns = async () => {
       try {
         // Assuming you have a method called getCampaigns in your smart contract
         const campaignResults = await fundraisa_backend.getCampaigns();
-        console.log(campaignResults);
         // The actual implementation depends on how your smart contract exposes the campaigns
         // For example, if getCampaigns returns an array of campaigns, you can set the state directly
         setCampaigns(campaignResults.map(result => result[1]));
@@ -64,50 +71,99 @@ const Landing = () => {
     fetchCampaigns();
   }, []);
 
+  const handleSignIn = async () => {
+    try {
+      // signin using internet identity
+      await authClient.login({
+        identityProvider: process.env.II_URL,
+        onSuccess: () => {
+          // Redirect to the landing page after signing in
+          navigate("/profile");
+        },
+        onError: (error) => {
+          console.error("Failed to sign in:", error);
+        },
+        // ...
+      });
+    }
+    catch (error) {
+      console.error("Failed to sign in:", error);
+    }
+  }
   return (
-    <div className="flex flex-col items-center gap-4 pt-12">
-      <img src="fundraisa_logo.png" alt="Logo" className="logo" width="100" />
-      <div className="text-center space-y-4">
-        <div className="space-y-1">
-          <h1 className="font-semibold text-3xl">Welcome to Fundraisa</h1>
-          <p>Let's make fundraising easy!</p>
-        </div>
-        <Button>
-          <Link to="/addUser">Signup</Link>
-        </Button>
+    <div className="flex flex-col items-center gap-4 pt-12" role="navigation">
+    <img src="fundraisa_logo.png" alt="Fundraisa Logo" className="logo" width="100" />
+    <div className="text-center space-y-4" role="main">
+      <div className="space-y-1">
+        <h1 className="font-semibold text-3xl">Welcome to Fundraisa</h1>
+        <p>Let's make fundraising easy!</p>
       </div>
-      <div className="text-sm text-center">
-        <h2>Available Campaigns</h2>
-        {campaigns.length > 0 ? (
-          <div className="flex flex-row flex-wrap justify-between items-stretch gap-4">
-            {campaigns.map((campaign) => {
-              return (
-            <Card className="w-200 h-250" key={campaign.campaignId}>
-              <CardHeader>
-              <h3 className="font-semibold">{campaign.campaignTitle}</h3>
-              </CardHeader>
-              <CardContent>
-              <p>{campaign.campaignDescription}</p>
-              <p>Goal: {campaign.campaignGoal}</p>
-              <Button>Donate</Button>
-              </CardContent>
-              <CardFooter>
-              <p>Ends: {campaign.campaignEndDateTime}</p>
-              </CardFooter>
-            </Card>
-          );
-        })}
-          </div>
-        ) : (
-          <p>No campaigns available yet.</p>
-        )}
-      </div>
+      <Button onClick={handleSignIn}>SignIn
+      </Button>
     </div>
+    <div className="text-sm text-center">
+      <h2>Available Campaigns</h2>
+      {campaigns.length > 0 ? (
+        <div className="flex flex-row flex-wrap justify-between items-stretch gap-4">
+          {campaigns.map((campaign, index) => {
+            return (
+          <Card className="bg-white shadow-xl rounded-lg w-96 max-w-md h-96 
+          my-4 p-4 flex flex-col justify-between" 
+ key={campaign.campaignId}>
+          <div className="flex flex-col items-center gap-4">
+            <CardTitle className="font-semibold">{campaign.campaignTitle}</CardTitle>
+            <CardContent>
+            <p id={`card-describe-${index}`}>{campaign.campaignDescription}</p>
+            <p>Goal: {campaign.campaignGoal}</p>
+            <Button aria-describedby={`card-describe-${index}`} aria-label={`Donate to ${campaign.campaignTitle}`}>Donate</Button>
+            </CardContent>
+            <CardFooter>
+            <p>Ends: {campaign.campaignEndDateTime}</p>
+            </CardFooter>
+          </div>
+          </Card>
+        );
+          })}
+        </div>
+      ) : (
+        <p>No campaigns available yet.</p>
+      )}
+    </div>
+  </div>
   );
 };
 
+// sign in using internet identity
+const signin = () => {
+  const navigate = useHistory();
+  const handleSignIn = async () => {
+    try {
+      // signin using internet identity
+      await authClient.login({
+        identityProvider: process.env.II_URL,
+            onSuccess: resolve,
+            onError: reject,
+            // ...
+      });
+      // Redirect to the landing page after signing in
+      navigate("/profile");
+    }
+    catch (error) {
+      console.error("Failed to sign in:", error);
+    }
+  }
+  return (
+    <div className="flex flex-col items-center gap-4 pt-12">
+      <img src="fundraisa_logo.png" alt="Logo" className="logo" width="100" />
+      <h1 className="font-semibold text-3xl">Signin</h1>
+      <Button onClick={handleSignIn}>Signin</Button>
+    </div>
+  );
+}
+  
+
 // sign up
-const AddUser = () => {
+const profile = () => {
   const navigate = useHistory();
   const [username, setUsername] = useState("");
   const [ NationalID, setNationalID] = useState("");
@@ -257,7 +313,8 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/addUser" element={<AddUser />} />
+        <Route path="/signin" element={<signin />} />
+        <Route path="/profile" element={<profile />} />
         <Route path="/createCampaign" element={<CreateCampaign />} />
         <Route path="/campaign/:campaignId" element={<Campaign />} />
         <Route path="/donate/:campaignId" element={<Donate />} />
